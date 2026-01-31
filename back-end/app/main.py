b6752bfa6cdbc9dfd2e48980bb5a5684187a514a
laura-ferreira2 <laura.ferreira@ufba.br>
2026-01-31 18:36:14 -0300

    fix: cors vercel authorization

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",   
-        "https://emailai.vercel.app"  
+        "https://email-ai-fawn.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
